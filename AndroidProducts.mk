1c0b10f0984cc40d0bd266d908289c59c31c8f73
TegarXLu <botdimas67@gmail.com>
2025-04-28 06:34:07 +0700

    Initial Tree For X6827

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X6815D.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X6815D-user \
+    twrp_X6815D-userdebug \
+    twrp_X6815D-eng
