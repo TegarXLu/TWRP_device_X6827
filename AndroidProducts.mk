928c87def00c7515acb57d991cbe9e479b926469
TegarXLu <botdimas67@gmail.com>
2025-04-28 11:36:41 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_X6827.mk
+    $(LOCAL_DIR)/twrp_GarudaX.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_X6827-user \
-    twrp_X6827-userdebug \
-    twrp_X6827-eng
+    twrp_GarudaX-user \
+    twrp_GarudaX-userdebug \
+    twrp_GarudaX-eng
